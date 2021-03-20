a080d5bb50a61229bd7ce59aacb26e65a0e90bcf
neven-h <69196529+neven-h@users.noreply.github.com>
2021-03-20 12:39:49 +0200

    Update and rename my_sin_edited.c to my_sin.c

--- my_sin.c
+++ my_sin.c
@@ -1,26 +1,29 @@
 /* this one is working when the input is in radians*/
+
 #include <stdio.h>
 #include <math.h>
+
 #define PI 3.14159265358979323846
 #define EPSILON 0.000000001
+#define Y 6.150444064011
 
 /* my abslute value function */
-double myabs(double x){
-    if(x>= 0) return x; /* if x is positive, return x as is */
-    else
+double myabs(double x) {
+  if (x >= 0) return x; /* if x is positive, return x as is */
+  else
     return (-x); /*if x is negetive, change it to be positive */
 }
 
 /*the function my_sin */
-  double my_sin(double x) {
+double my_sin(double x) {
   /*declations: */
-  double term, total_so_far,a, b, c;
-  term = x;  /* First term in the expansion.*/
+  double term, total_so_farת b, c;
+  term = x; /* First term in the expansion.*/
   total_so_far = 0.00;
-  a = 1, b=0.0, c=1.0;
-  
+  b = 0.0, c = 1.0;
+
   /*if the last term in the sum is smaller then EPSILON, return the result */
-  while(myabs(term)> EPSILON){
+  while (myabs(term / 2) >= EPSILON) {
     /* Add current term to sum. */
     total_so_far = total_so_far + term;
     /* Compute next term from the current one. */
@@ -32,22 +35,30 @@ double myabs(double x){
 }
 
 int main(void) {
-  double x,res_my_sin, res_system_sin;
+  double x, res_my_sin, res_system_sin;
   double my_sin(double x);
 
-    printf("Please wnter a value in the range [-25.0,25,0] which you'd like to be computed: ");
-    scanf("%lf", &x);
-    if(myabs(x)>25.0){
+  printf("Please wnter a value in the range [-25.0,25,0] which you'd like to be computed: ");
+  scanf("%lf", & x);
+  if (myabs(x) > 25.0) {
     printf("the value you entered is not in the legel range, please try again");
-    }
+  }
+  /* manuel solution for the problem when the input is 25 or -25 */
+  if (myabs(x) == 25) {
+    if (x > 0) res_my_sin = my_sin(Y);
+    else res_my_sin = my_sin(-Y);
+  }
+  /* if the input is legel: */
+  else {
     res_my_sin = my_sin(x);
-    /* if x is in the legal values of [-25,25], then sin(x) will be bounded by -1 and 1*/
-    if(myabs(res_my_sin) <= 1){
+  }
+  /* if x is in the legal values of [-25,25], then sin(x) will be bounded by -1 and 1*/
+  if (myabs(res_my_sin) <= 1) {
     printf("Using my_sin function sin(%f) is %f\n", x, res_my_sin);
     res_system_sin = sin(x);
     printf("\nUsing the C library function sin(%f) = %f\n", x, res_system_sin);
-    }
-    /* if x is not in legel values */
+  }
+  /* if x is not in legel values, then we will have a stack overflow error, and for that case we wont compute sin using my_sin */
   else {
     printf("the value you entered is not in the legel range, please try again");
   }
