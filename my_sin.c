935d27fbf96101f5562c7be7aa66956d9367ab1e
neven-h <69196529+neven-h@users.noreply.github.com>
2021-03-21 18:06:10 +0200

    Update my_sin.c

--- my_sin.c
+++ my_sin.c
@@ -1,21 +1,39 @@
-/* this one is working when the input is in radians*/
-
 #include <stdio.h>
 #include <math.h>
 
 #define PI 3.14159265358979323846
 #define EPSILON 0.000000001
 #define Y 6.150444064011
+#define UNITCIRCLE 360
+#define DEGREESINRAD 57.29577951308232087684636434419109775
 
 /* my abslute value function */
-double myabs(double x) {
-  if (x >= 0) return x; /* if x is positive, return x as is */
+double my_abs(double x)
+{
+  if (x >= 0)
+    return x; /* if x is positive, return x as is */
   else
     return (-x); /*if x is negetive, change it to be positive */
 }
 
+/*Converts the inputs to the first 365 degrees, to avoid aritmetic overflow*/
+double convert_input_to_range(double x)
+{
+  double y = 1 / DEGREESINRAD;      /*y is the number of radians in 1 degree*/
+  double z = y * UNITCIRCLE; /*z is the number of radians in 360 degrees*/
+  while (my_abs(x) >= z)         /*Converts the input to the first 360 degrees, to avoid stack overflow*/
+  {
+    if (x < 0)
+      x += z;
+    if (x > 0)
+      x -= z;
+  }
+  return x;
+}
+
 /*the function my_sin */
-double my_sin(double x) {
+double my_sin(double x)
+{
   /*declations: */
   double term, total_so_far, b, c;
   term = x; /* First term in the expansion.*/
@@ -23,7 +41,8 @@ double my_sin(double x) {
   b = 0.0, c = 1.0;
 
   /*if the last term in the sum is smaller then EPSILON, return the result */
-  while (myabs(term / 2) >= EPSILON) {
+  while (my_abs(term / 2) >= EPSILON)
+  {
     /* Add current term to sum. */
     total_so_far = total_so_far + term;
     /* Compute next term from the current one. */
@@ -34,33 +53,21 @@ double my_sin(double x) {
   return total_so_far;
 }
 
-int main(void) {
+int main(void)
+{
+  /*declations: */
   double x, res_my_sin, res_system_sin;
   double my_sin(double x);
+  double my_abs(double x);
+  double convert_input_to_range(double x);
 
   printf("Please wnter a value in the range [-25.0,25,0] which you'd like to be computed: ");
-  scanf("%lf", & x);
-  if (myabs(x) > 25.0) {
-    printf("the value you entered is not in the legel range, please try again");
-  }
-  /* manuel solution for the problem when the input is 25 or -25 */
-  if (myabs(x) == 25) {
-    if (x > 0) res_my_sin = my_sin(Y);
-    else res_my_sin = my_sin(-Y);
-  }
-  /* if the input is legel: */
-  else {
-    res_my_sin = my_sin(x);
-  }
-  /* if x is in the legal values of [-25,25], then sin(x) will be bounded by -1 and 1*/
-  if (myabs(res_my_sin) <= 1) {
-    printf("Using my_sin function sin(%f) is %f\n", x, res_my_sin);
-    res_system_sin = sin(x);
-    printf("\nUsing the C library function sin(%f) = %f\n", x, res_system_sin);
-  }
-  /* if x is not in legel values, then we will have a stack overflow error, and for that case we wont compute sin using my_sin */
-  else {
-    printf("the value you entered is not in the legel range, please try again");
-  }
+  scanf("%lf", &x);
+  x = convert_input_to_range(x); /* converts the input to avoid mistakes in calculation as a result of arithmetic overflow*/
+  res_my_sin = my_sin(x); /* assigning a variable to the function as we don't want to call the function directly*/
+  printf("Using my_sin function sin(%f) =  %f\n", x, res_my_sin);
+  res_system_sin = sin(x);/* assigning a variable to the function as we don't want to call the function directly*/
+  printf("\nUsing the C library function sin(%f) = %f\n", x, res_system_sin);
+
   return 0;
 }
